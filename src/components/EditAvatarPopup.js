3960b511bdfcade202fbfa0535169cc431aa5af3
Aleksandr Polyakov <aleksandrpoliakoff@yandex.ru>
2023-04-09 14:25:25 +0300

    clearing the input in EditAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,37 +1,43 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
 
   const avatarRef = useRef()
 
-    function handleSubmit(e) {
+  function handleSubmit(e) {
 
-        e.preventDefault();
+    e.preventDefault();
 
-        onUpdateAvatar(
-            avatarRef.current.value
-        );
+    onUpdateAvatar(
+      avatarRef.current.value
+    );
+  }
+
+  useEffect(() => {
+    if (!isOpen) {
+      avatarRef.current.value = ''
     }
+  }, [isOpen])
 
   return (
     <PopupWithForm
-            title='Обновить аватар'
-            name='edit-avatar'
-            isOpen={isOpen}
-            onClose={onClose}
-            buttonText='Сохранить'
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="popup__input popup__input_form_image"
-              type="url"
-              name="avatar"
-              placeholder="Ссылкка на картинку"
-              required
-              ref={avatarRef}/>
-            <span className="popup__error avatar-error"></span>
-          </PopupWithForm>
+      title='Обновить аватар'
+      name='edit-avatar'
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText='Сохранить'
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="popup__input popup__input_form_image"
+        type="url"
+        name="avatar"
+        placeholder="Ссылкка на картинку"
+        required
+        ref={avatarRef} />
+      <span className="popup__error avatar-error"></span>
+    </PopupWithForm>
   );
 }
 
